abf0efb9fc840276b333ec49e6d925036ed4a1f1
Bryana <bryananwall@gmail.com>
2019-03-01 10:36:45 -0600

    Add javascript functionality to site

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+I had quite a few a few problems with this assignment. I had trouble using the getElementsByTagName and getElementsByClassName at all. 
+
+For resources this youtube video helped me a lot https://www.youtube.com/watch?v=bdft2MZEqJY
+
+
 # m8-hw8-wall-bryana
--- hw8-starter-files/main.js
+++ hw8-starter-files/main.js
@@ -1,1 +1,81 @@
+document.getElementById("main-menu-labels1").addEventListener("mouseenter", openMenu);
+
+document.getElementById("main-menu-labels1").addEventListener("mouseleave", openMenu);
+
+function openMenu(){
+    document.getElementById("sub-menu1").classList.toggle("active");
+}
+
+document.getElementById("main-menu-labels2").addEventListener("mouseenter", openMenu1);
+
+document.getElementById("main-menu-labels2").addEventListener("mouseleave", openMenu1);
+
+function openMenu1(){
+    document.getElementById("sub-menu2").classList.toggle("active");
+}
+
+document.getElementById("main-menu-labels3").addEventListener("mouseenter", openMenu2);
+
+document.getElementById("main-menu-labels3").addEventListener("mouseleave", openMenu2);
+
+function openMenu2(){
+    document.getElementById("sub-menu3").classList.toggle("active");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // It is always helpful to use comments in your code!
+
+//var hide = document.getElementById("sub-menu");
+
+//var products = document.getElementsByClassName("main-menu-labels")[0];
+
+//document.getElementsByTagName("main-menu-labels")[0].addEventListener("click", openMenu);
+
+//function openMenu(){
+    
+
+//document.getElementById("sub-menu").classList.toggle("active");
+    
+//}
+
+/*
+
+document.getElementsByTagName("main-menu-labels").addEventListener("mouseover", mouseOver);
+
+function mouseOver(){
+    
+    document.getElementById("sub-menu").classList.toggle("active");
+}
+
+
+function mouseOver(){
+    document.getElementById("main-menu-labels").style.visibility = "visible";
+}
+
+
+products.addEventListener("mouseOver", function(){
+    hide.style.visibility ="visible";
+});
+
+
+var myFunction = function() {
+    hide.removeAttribute("style");
+}
+
+products.onmouseover =  myFunction;*/
